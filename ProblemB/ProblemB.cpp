28816624ae51c2238d99797c16c4d41c64a95604
Denis Czech <117384166+CzechU99@users.noreply.github.com>
2024-02-29 08:48:00 +0100

    Add files via upload

---
+++ ProblemB/ProblemB.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int l, u;
+
+	while (true) {
+
+		cin >> l >> u;
+
+		int licznik;
+		int licznik_x = 0;
+		int licznik_y = 0;
+
+		if (l == -1 && u == -1) {
+			break;
+		}
+		else if (l < 0 && u < 0) {
+		}
+		else {
+			if (u >= 0) {
+				for (int i = l; i <= u; i++) {
+					licznik = 0;
+					for (int x = 1; x <= i; x++) {
+						if (i % x == 0) {
+							licznik++;
+						}
+					}
+					if (licznik == 2) {
+						licznik_x++;
+						if ((i - 1) % 4 == 0) {
+							licznik_y++;
+						}
+					}
+				}
+			}
+		}
+
+		cout << l << "  " << u << "  " << licznik_x << "  " << licznik_y << '\n';
+
+	}
+
+}
